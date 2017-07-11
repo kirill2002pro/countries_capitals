863bf5180e03acde25a10b8ddbb8ddb5e064b591
Kirill2002pro <kirill@hancharou.com>
2017-07-11 15:06:59 -0400

    Initial commit

---
+++ myScript.js
@@ -0,0 +1,10 @@
+
+ var coco=[
+ ["Беларусь","Испания","Италия","Франция","Польша","Украина","США"],
+ ];
+ var text="";
+ var a;
+ var b;
+ for(a=0,b=0;a<coco.length+a++),b<coco.length+b++);{"<tr>"+"<td>"+text+=coco[a,b]+"</td>"+"</tr>";
+ } 
+ document.getElementById("Table1").inner HTML
